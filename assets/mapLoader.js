d586ce5d64e5273a6fc15ca28db9a6572d78a75d
Douglas Marinho <55254183+dglsmrnh@users.noreply.github.com>
2021-09-06 19:27:48 -0300

    init

---
+++ assets/mapLoader.js
@@ -0,0 +1,7 @@
+// Load map configuration https://github.com/googlemaps/js-api-loader
+var loader = new google.maps.plugins.loader.Loader({
+    apiKey: "AIzaSyDQWB7glO78XBtab5sdMb4I70quCR51q40",  //apiKey from Google 
+    version: "weekly",
+    language: 'pt-BR',
+    region: 'BR'
+});
---
+++ helloWord/map.js
@@ -0,0 +1,9 @@
+var map;
+
+// load the map object
+loader.load().then(() => {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+});
